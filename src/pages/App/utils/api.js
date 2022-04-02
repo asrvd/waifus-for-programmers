11ad61c223c3842fee34c46ddd5e87d8be68d547
Dust <73678906+TheRealDust@users.noreply.github.com>
2022-04-02 12:23:21 +1100

    Undid some formatting
    Code got auto formatted.

--- src/pages/App/utils/api.js
+++ src/pages/App/utils/api.js
@@ -1,18 +1,18 @@
 export function getfolders() {
     return fetch('https://api.github.com/repos/cat-milk/Anime-Girls-Holding-Programming-Books/contents/')
-        .then(res => res.json())
-        .then(data => {
-            const newData = data.map((folder) => (
-                {
-                    name: folder.name,
-                    path: folder.path,
-                    key: folder.name
-                }
-            ))
-            //console.log(newData)
-            //console.log(FolderData)
-            return newData
-        })
+    .then(res => res.json())
+    .then(data => {
+        const newData = data.map((folder) => (
+            {
+                name: folder.name,
+                path: folder.path,
+                key: folder.name
+            }
+        ))
+        //console.log(newData)
+        //console.log(FolderData)
+        return newData
+    })
 
 }
 
@@ -24,20 +24,20 @@ export async function getauthor(path) {
 
 export function getcont(path) {
     return fetch(`https://api.github.com/repos/cat-milk/Anime-Girls-Holding-Programming-Books/contents/${encodeURIComponent(path)}?ref=master`)
-        .then(res => res.json())
-        .then(data => {
-            //console.log(data)
-            const newData = data.map((file) => (
-                {
-                    name: file.name,
-                    url: file.download_url,
-                    key: file.name,
-                    path: file.path,
-                    author: getauthor(file.path),
-                }
-            ))
-            return newData
-        })
+    .then(res => res.json())
+    .then(data => {
+        //console.log(data)
+        const newData = data.map((file) => (
+            {
+                name: file.name,
+                url: file.download_url,
+                key: file.name,
+                path: file.path,
+                author: getauthor(file.path),
+            }
+        ))
+        return newData
+    })
 }
 
 //getfolders()
