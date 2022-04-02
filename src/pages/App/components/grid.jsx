c2d437fa903fcd9a8578dd4905d3d5a74421ff25
JamesDundon05 <73678906+JamesDundon05@users.noreply.github.com>
2022-04-02 11:54:16 +1100

    Author name on images
    Put the author's name in a box on the top right of each image

--- src/pages/App/components/grid.jsx
+++ src/pages/App/components/grid.jsx
@@ -8,32 +8,42 @@ export default class MainGrid extends Component {
     constructor(props) {
         super(props);
     }
-    
+
     // Credits: @TheRealDust
     componentDidUpdate() {
         const images = Array.from(document.querySelectorAll(".img"));
 
         // Apply skeleton to all image containers
-        images.map((image) => image.parentNode).filter((container) => !container.classList.contains("skeleton")).map((container) => container.classList.add("skeleton"));
+        const applyClassToContainer = name => images.map((image) => image.parentNode).filter((container) => !container.classList.contains(name)).map((container) => container.classList.add(name));
+
+        applyClassToContainer("skeleton");
+        applyClassToContainer("imageContainer");
 
         // Once all images are loaded remove the `hidden` class to display them
-        Promise.all(images.map((image) => new Promise((resolve) => image.onload = resolve))).then(() => {
+        Promise.all(images.map((image) => new Promise((resolve) => image.onload = resolve))).then(async () => {
+            // Fetch author data 
+            const authors = await Promise.all(this.props.data.map((data) => data.author));
+            // Patch live author data into data props
+            const patchedData = this.props.data.map((data, i) => {
+                const d = data;
+                d.author = authors[i];
+                return d;
+            });
+
             images.map((image) => {
                 image.classList.remove("hidden");
                 // Remove skeleton from container element
                 image.parentNode.classList.remove("skeleton");
+
+                // Find the data packet for this image
+                const data = patchedData.find(element => image.src == element.url);
+
+                // Display author over image
+                const authorDiv = document.createElement("div");
+                authorDiv.innerText = `@${data.author}`;
+                authorDiv.className = "authorDiv";
+                image.parentNode.appendChild(authorDiv);
             });
-
-            // Fetch author data
-            Promise.all(this.props.data.map((data) => data.author)).then((authors) =>
-                images.map((e, i) => [e, authors[i]] /* zip(images, authors) */).map(([image, author]) => {
-                    const authorSpan = document.createElement("span");
-                    authorSpan.innerText = `By ${author}`;
-                    authorSpan.className = "authorSpan";
-
-                    image.parentNode.appendChild(authorSpan);
-                }
-                ));
         });
     }
 
