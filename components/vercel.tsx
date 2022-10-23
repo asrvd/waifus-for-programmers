c2b73d8767e7da33f58bd95a6e1ba9b20182ddd6
asrvd <asheeshh@duck.com>
2022-10-23 12:10:58 +0530

    fix: badge url

--- components/vercel.tsx
+++ components/vercel.tsx
@@ -2,7 +2,7 @@
 
 export default function PoweredByVercel() {
   return (
-    <a href="https://vercel.com?utm_source=waifus-for-programmers&utm-campaign=oss">
+    <a href="https://vercel.com?utm_source=waifus-for-programmers&utm_campaign=oss">
       <img src="https://raw.githubusercontent.com/abumalick/powered-by-vercel/master/powered-by-vercel.svg" alt="powered-by-vercel"/>
     </a>
   );
